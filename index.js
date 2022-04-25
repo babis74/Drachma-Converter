bf0d87caf0500c77c35a7672cd3f6918e3833b75
babis74 <hsdrallis@gmail.com>
2022-04-25 23:40:45 +0300

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+// Η Συνάρτηση  της εφαρμογής 
+
+function double()
+{
+  num1 = document.getElementById("firstNumber").value;       
+  document.getElementById("demo").innerHTML = num1 * 340,75;
+  document.getElementById("thanxs").innerHTML="Σας ευχαριστώ που χρησιμοποιήσατε την εφαρμογή μου!";
+}
+
+//Θα πρέπει να κατασκευασω και μια συνάρτηση που να μετράει τις φορές
+//που χρησιμοποιείτε η παραπάνω συνάρτηση
